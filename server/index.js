fc612a614073af3b10c994b02cbebdff95b3833d
Bergson Melo <bergsonmelo11@gmail.com>
2025-09-24 00:14:24 -0300

    mais um

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const { Server } = require("peer");
-const cors = require("cors");
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // PeerJS Server
-const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(app, {
+  path: '/peerjs',
   debug: true,
 });
 
-app.use("/peerjs", peerServer);
+app.use('/peerjs', peerServer);
 
-app.get("/", (req, res) => res.send("PeerJS Server Running"));
+app.get('/', (req, res) => res.send('PeerJS Server Running'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
